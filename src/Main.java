77ed2dff5b090166ff7c3f496e27dee91d28f631
Sergey Davydov <samd82@yandex.ru>
2021-02-15 04:12:09 +0500

    Выполнены все пункты домашнего задания к уроку 2!!!

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -16,10 +18,27 @@ public class Main {
     //Вызов метода для задания 5.
         minmaxArr();
 
+    //Вызов метода для задания 6.
+
+    int [] massBalance = {2, 3, 8, 1, 1, 7, 3, 3};
+    boolean balance = checkBalance(massBalance);
+    System.out.println("Результат логической обработки задания 6: "+balance);
+    System.out.println("-----------------");
+
+
+    // Вызов метода для задания 7.
+        int[] shiftArr = {5,6,9,1};
+        int n = 2; //если число n положительное, то сдвиг массива вправо, если отрицательное, то влево
+        shiftNumbers(shiftArr, n);
+
+
+
+
     }
 //Задание 1. Задать массив. Инвертировать значения.
     static void integerMassive1()
     {
+        System.out.println("Задание 1:");
         int[] arr= new int[10];
         arr[0]=1;
         arr[1]=1;
@@ -31,7 +50,10 @@ public class Main {
         arr[7]=1;
         arr[8]=0;
         arr[9]=1;
-
+        System.out.println("Исходное значение массива:");
+        System. out .println(Arrays. toString (arr));
+        System.out.println("Полученное значение массива:");
+        System.out.print("[ ");
 
         for (int index=0; index < arr.length; index++)
         {
@@ -40,14 +62,20 @@ public class Main {
                 arr[index]=0;
             }
             else arr[index]=1;
-            System.out.println("Массив1 "+"["+index+"] = "+ arr[index]);
+            System.out.print(arr[index] + ", ");
         }
+        System.out.println("]");
+        System.out.println("-----------------");
     }
 //Задание 2. Задать массив размером 8. Заполнить рядом чисел.
     static void integerMassive2()
     {
         int[] arr= new int[8];
-
+        System.out.println("Задание 2:");
+        System.out.println("Исходное значение массива:");
+        System. out .println(Arrays. toString (arr));
+        System.out.println("Полученное значение массива:");
+        System.out.print("[ ");
 
         for (int index=0; index < arr.length; index++)
         {
@@ -58,13 +86,20 @@ public class Main {
             else
             arr[index]=arr[index-1]+3;
 
-            System.out.println("Массив2 "+"["+index+"] = "+ arr[index]);
+            System.out.print(arr[index] + ", ");
         }
+        System.out.println("]");
+        System.out.println("-----------------");
     }
 //Задание 3. Создать массив. Значения массива меньше 6 умножить на 2
     static void integerMassive3()
     {
         int[] arr= {1,5,3,2,11,4,5,2,4,8,9,1};
+            System.out.println("Задание 3:");
+        System.out.println("Исходное значение массива:");
+        System. out .println(Arrays. toString (arr));
+        System.out.println("Полученное значение массива:");
+        System.out.print("[ ");
 
         for (int index=0; index < arr.length; index++)
         {
@@ -73,15 +108,17 @@ public class Main {
                 arr[index]=arr[index]*2;
             }
 
-            System.out.println("Массив3 "+"["+index+"] = "+ arr[index]);
+            System.out.print(arr[index]+", ");
         }
+        System.out.println("]");
+        System.out.println("-----------------");
     }
 
 //Задание 4. Двумерный квадратный массив. Диагональ заполнить единичками
     static void twoArr1()
     {
         int twoArr[][] = new int[5][5];
-
+            System.out.println("Задание 4: Заполняем диагонали единичками");
         for (int i = 0; i < twoArr.length; i++ )
         {
             for (int j = 0; j < twoArr[i].length;j++)
@@ -95,37 +132,153 @@ public class Main {
                 System.out.print(twoArr[i][j]+" ");
             }
             System.out.println();
+
         }
+        System.out.println("-----------------");
     }
 
 //Задание 5**. Одномерный массив с вычислением минимального и максимального числа
     static void minmaxArr()
     {
-        int[] arr = {16, 3, 5, 7, 100, 0, 9, 15, 11, 40};
-        int maximumVal = arr[0];
-        int minimumVal = arr[0];
+        System.out.println("Задание 5**:");
+
+        int[] arr = {5, 2, 5, 7, 44, 1, 200, 5, 11, 40};
+        int maximumVal=arr[0];
+        int minimumVal=arr[0];
+
+        System.out.println("Исходное значение массива:");
+        System. out .println(Arrays. toString (arr));
+
 
         for (int i = 0; i < arr.length; i++)
         {
+            //прогоняем вложенным циклом сравнение каждого с каждым
+            for (int j = 0; j< arr.length; j++)
+            {
+                if (arr[j] > arr[i] && arr[j]>maximumVal) //двойное условие для фиксации максимального значения
+                {
+                maximumVal = arr[j];
+                }
 
-       if (i != 0)
+                if (arr[j] < arr[i] && arr[j] < minimumVal) //двойное условие для фиксации минимального значения
+                {
+                    minimumVal = arr[j];
+                }
+            }
+
+        }
+        System.out.println("Максимальное значение архива: "+maximumVal);
+        System.out.println("Минимальное значение архива: "+minimumVal);
+        System.out.println("-----------------");
+        }
+
+
+    // Задание 6**. Массив с проверкой сумм двух частей массива
+
+    static boolean checkBalance(int[] a)
+    {
+        System.out.println("Задание 6**:");
+        int y=a.length -1; //длина массива -1, определение номера последнего члена массива
+
+        boolean equal = false;
+
+        for (int i = 0; i<a.length; i++) //рассматриваем (длина массива - 1) случаев возможных комбинаций
         {
-                int j = i - 1;
-
-                if (arr[i] > arr[j] && arr[i] > arr[0]) //Вычисление максимального значения члена массива
-                {
-                    maximumVal = arr[i];
-                }
-                if (arr[i] < arr[j] && arr[i] < arr[0]) // Вычисление минимального значения члена массива
-                {
-                    minimumVal = arr[i];
-                }
+            int sumLeft = 0;
+            int sumRight = 0;
+            int j = 0;
+            while (j<=i)
+            {
+               sumLeft = a[j] + sumLeft;
+               j++;
             }
 
+            int z = i+1;
+            for (int rightInd = z; rightInd<a.length; rightInd++ )
+            {
+                sumRight = a[rightInd]+sumRight;
             }
-        System.out.println(maximumVal);
-        System.out.println(minimumVal);
+            if (sumLeft == sumRight)
+            {
+                System.out.println("Суммы левой и правой части равны");
+                System.out.println("Крайний индекс левой части массива: "+i);
+                System.out.print("[ ");
+                for (int p = 0; p <=i; p++)
+                {
+                    System.out.print(a[p]+ " ");
+
+                }
+                System.out.print("|| ");
+                for (int p2 = i; p2<a.length; p2++)
+                {
+                    System.out.print(a[p2]+ " ");
+                }
+                System.out.print("]");
+                System.out.println();
+               equal = true;
+
+            }
+        }
+
+
+
+        return (equal);
+    }
+
+// Задание 7***. Сдвиг членов массива на значение n
+
+static void shiftNumbers(int[] a, int n)
+    {
+        System.out.println("Задание 7***: Сдвинуть члены массива на n позиций");
+        System.out.println("n = "+n);
+        System.out.println("Исходное значение массива:");
+        System. out .println(Arrays. toString (a));
+        System.out.println("Полученное значение массива:");
+
+
+        int y = a.length -1; //определение крайнего правого положения в массиве
+        int newPos =0;
+
+    if (n>0) //сдвиг вправо
+    {
+           for (int j = 1; j<=n; j++)
+            {
+                int tempVal = a[y]; //записываем крайний правый член массива во временную переменную
+                for (int i=y; i>0; i--)
+                {
+                    a[i] = a[i-1]; //сдвиг на одну позицию
+                }
+                a[0] = tempVal; // запись нулевого члена из временной переменной
+
+            }
+    }
+    else if (n<0) //сдвиг влево
+    {
+        for (int j = -1; j >= n; j--)
+        {
+            int tempVal = a[0]; //записываем крайний правый член массива во временную переменную
+            for (int i = 0; i < y; i++)
+            {
+                a[i] = a[i + 1]; //сдвиг на одну позицию
+            }
+            a[y] = tempVal; // запись нулевого члена из временной переменной
+        }
+    }
+        System. out .println(Arrays. toString (a));
+        System.out.println("-----------------");
+    }
+
 
     }
-}
+
+
+
+
+
+
+
+
+
+
+
 
