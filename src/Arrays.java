7737cfcc145c5d3ea8be8003c9ce2280ac9127d6
Sergey Davydov <samd82@yandex.ru>
2021-04-01 21:57:48 +0500

    Выполнено домашнее задание к уроку 5 (Многопоточность)

--- src/Arrays.java
+++ src/Arrays.java
@@ -6,12 +6,11 @@ public class Arrays {
     float [] arr = new float[SIZE];
 
 
-    //заполнение единичками массива
+    //заполнение формулами по первому методу массива
     public void arrayFill1 () {
         long a = System.currentTimeMillis();
         for (int i=0; i <arr.length; i++) {
-            arr[i] = 1;
-
+            arr[i] = (float) (arr[i] * Math.sin(0.2f+i/5) * Math.cos(0.2f +i/5) * Math.cos(0.4f + i/2));
         }
         long b = System.currentTimeMillis();
         long c = b-a;
@@ -26,19 +25,21 @@ public class Arrays {
         float [] a2 = new float [HALF];
 
         long a = System.currentTimeMillis(); //засекаем исходную точку времени
-
+        System.arraycopy(arr,0, a1,0, HALF); //заполняем первый массив значениями исходного
+        System.arraycopy(arr,HALF, a2,0,HALF); // заполняем второй массив значениями исходного
+        long g = System.currentTimeMillis(); //время окончания разделения массива и начала первого потока
         //первый поток
         Thread t1 =new Thread(()->{
-            System.arraycopy(arr,0, a1,0, HALF); //заполняем первый массив значениями исходного
+
             for (int i=0; i<HALF; i++) {
                 a1[i] = (float) (arr[i] * Math.sin(0.2f+i/5) * Math.cos(0.2f +i/5) * Math.cos(0.4f + i/2));
             }
         });
-
+        long d = System.currentTimeMillis(); //время окончания первого потока и начала второго
 
         //второй поток
         Thread t2 =new Thread(()->{
-            System.arraycopy(arr,HALF, a2,0,HALF); // заполняем второй массив значениями исходного
+
             for (int i=0; i<HALF; i++) {
                 a2[i] = (float) (arr[i] * Math.sin(0.2f+i/5) * Math.cos(0.2f +i/5) * Math.cos(0.4f + i/2));
             }
@@ -46,13 +47,26 @@ public class Arrays {
         t1.start(); //запуск первого потока
         t2.start(); //запуск воторого потока
 
+        long e = System.currentTimeMillis(); // время начала склейки
+
+
         System.arraycopy(a1,0, arr,0,HALF); //заполняем исходный массив значениями первого расчётного массива
         System.arraycopy(a2,0, arr,HALF,HALF ); //заполняем вторую часть исходного массива значениями второго расчётного массива
 
 
         long b = System.currentTimeMillis();
-        long c = b-a; //вычисляем время выполнения метода
-        System.out.println("Время заполнения массива формулами при помощи 2-х потоков: "+c+" мс");
+        long h = g-a; //вычисляем время разделения массива
+        long i = d-g; //вычисляем время первого потока
+        long j = e-d; //вычисляем время второго потока
+        long o = b-e; //вычисляем время склейки
+        long f = b-a; //вычисляем время выполнения метода
+
+
+        System.out.println("Время разделения массива: "+h+" мс");
+        System.out.println("Время первого потока: "+i+" мс");
+        System.out.println("Время 2-го потока: "+j+" мс");
+        System.out.println("Время склейки: "+o+" мс");
+        System.out.println("Время метода: "+f+" мс");
     }
 
     }
